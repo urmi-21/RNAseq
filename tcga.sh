bc51abb0c139bb99c991fae200bbbf97e20cbbbe
Urminder Singh <mrbains21@gmail.com>
2018-11-01 17:08:14 -0500

    Create tcga.sh

---
+++ tcga.sh
@@ -0,0 +1,7 @@
+#get tumor and non tumor samples
+
+touch tumorSamps
+for f in $(ls *tcga-t.txt); do head -1 $f | tr "\t" "\n" >> tumorSamps; done
+
+touch notumorSamps
+for f in $(ls *tcga.txt); do head -1 $f | tr "\t" "\n" >> notumorSamps; done
