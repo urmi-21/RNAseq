e46626c90db324b6b2a8f17342c065cd8cc66d1a
Urminder Singh <mrbains21@gmail.com>
2018-11-01 17:10:16 -0500

    Update tcga.sh

--- tcga.sh
+++ tcga.sh
@@ -4,4 +4,5 @@ touch tumorSamps
 for f in $(ls *tcga-t.txt); do head -1 $f | tr "\t" "\n" >> tumorSamps; done
 
 touch notumorSamps
-for f in $(ls *tcga.txt); do head -1 $f | tr "\t" "\n" >> notumorSamps; done
+for f in $(ls *tcga.txt); do head -1 $f | tr "\t" "\n" | tail -n +2 >> notumorSamps; done
+
