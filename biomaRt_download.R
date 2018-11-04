71e5f1f90dc2e251c490a0b889c44bdac9093e65
Urminder Singh <mrbains21@gmail.com>
2018-11-04 14:59:42 -0600

    Update biomaRt_download.R

--- biomaRt_download.R
+++ biomaRt_download.R
@@ -1,1 +1,25 @@
+#if (!requireNamespace("BiocManager", quietly = TRUE)){
+#  install.packages("BiocManager")
+#}
+#  
+#BiocManager::install("biomaRt", version = "3.8")
+library(readr)
+library(biomaRt)
+allNormalizedTCGA <- read_csv("allNormalizedTCGA.csv")
+
+
+geneSymbols<-allNormalizedTCGA$Hugo_Symbol
+write.csv(geneSymbols,"genesSymbols.csv",row.names=F)
+
+mart = useMart("ensembl", dataset="hsapiens_gene_ensembl")
+#see attribute and filter list
+attlist<-listAttributes(mart = mart)
+filterlist<-listFilters(mart)
+
+go=c("GO:0051330","GO:0000080","GO:0000114","GO:0000082")
+chrom=c(17,20,"Y")
+#list of attributes to get back
+myAttlist<-c("ensembl_gene_id","description","chromosome_name","start_position","end_position","strand","transcript_count","percentage_gene_gc_content","gene_biotype","source","go_id","name_1006","definition_1006","go_linkage_type","namespace_1003","goslim_goa_accession","goslim_goa_description","arrayexpress","hgnc_symbol","kegg_enzyme")
+geneMetadata<-getBM(attributes= myAttlist,filters=c("hgnc_symbol"),values=list(geneSymbols), mart=mart)
+
 
