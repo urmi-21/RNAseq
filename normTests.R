8623f25906e07d5afdaac41d54141c350800d18e
Urminder Singh <mrbains21@gmail.com>
2018-11-07 00:59:59 -0600

    Update normTests.R

--- normTests.R
+++ normTests.R
@@ -3,6 +3,7 @@ library(readr)
 library(edgeR)
 library(tidyverse)
 library(magrittr)
+library(ggplot2)
 setwd("C:/Users/mrbai/Desktop/normrandomTest")
 memory.limit(size=56000)
 
@@ -32,16 +33,121 @@ tpmCols<- names(df)[grepl("Name|TPM",names(df))]
 df_TPM<-df[,tpmCols]
 
 #plot histograms for tpms
+#ggplot(stack(df_TPM[1:194]), aes(x = ind, y = values)) +  geom_boxplot()
 
 
 
 
 
-read_tsv("SRR073758")%>%set_colnames(c("cyl", "disp_mean", "hp_mean"))
 
-which(is.na(df))
+# function to produce summary statistics (mean and +/- sd), as required for ggplot2
+data_summary <- function(x) {
+  mu <- mean(x)
+  med<-median(x)
+  #print(paste("mean is",log(mu)))
+  return(c(y=log(mu),ymax=log(med),ymin=log(med)))
+}
+
+getlogmax<- function(x){
+  return(c(y=log(max(x))))
+}
+
+getlogmin<- function(x){
+  return(c(y=log(min(x))))
+}
+
+newdf<-df_TPM[,2:3]+1
+
+ggplot(data=stack(newdf), aes(x=ind, y=(values))) + geom_crossbar(stat="summary", fun.y=data_summary, fun.ymax=getlogmax, fun.ymin=getlogmin) 
+
+ggplot(data=stack(newdf), aes(x=ind, y=log(values))) +   geom_violin() 
 
 
+plotlist = list()
+k=1
+for(i in seq(2, dim(df_TPM)[2], by = 200)){
+  df_s<-df_TPM[,c(i:min(i+200,dim(df_TPM)[2]))]+1
+   #p<-ggplot(stack(df_s), aes(x = ind, y = log(values))) +  geom_boxplot(aes(middle = mean(values)),coef = 6) +theme(axis.text.x = element_text(angle = 90, hjust = 1))
+   p<-ggplot(data=stack(df_s), aes(x=ind, y=(values))) + geom_crossbar(stat="summary", fun.y=data_summary, fun.ymax=getlogmax, fun.ymin=getlogmin) +theme(axis.text.x = element_text(angle = 90, hjust = 1))
+   plotlist[[k]]=p
+  k=k+1
+}
+
+pdf("RawTPMPlots.pdf")
+for (i in 1:length(plotlist)){
+  write(i, stderr())
+  print(plotlist[[i]])
+}
+dev.off()
+
+#sort by depth and plot
+bases <- read_delim("bases.txt", "\t", escape_double = FALSE,trim_ws = TRUE)
+bases<-bases %>% mutate(srr=paste("TPM_",run_accession,sep = ""))
+bases <- bases[bases$srr %in% names(df_TPM),]
+bases <- bases[order(bases$bases),]
+
+df_TPM_sorted<-df_TPM[,bases$srr]
+
+plotlist = list()
+k=1
+for(i in seq(2, dim(df_TPM_sorted)[2], by = 50)){
+  df_s<-df_TPM_sorted[,c(i:min(i+50,dim(df_TPM_sorted)[2]))]+1 #add 1 to avoid inf
+  p<-ggplot(data=stack(df_s), aes(x=ind, y=(values))) + geom_crossbar(stat="summary", fun.y=data_summary, fun.ymax=getlogmax, fun.ymin=getlogmin) +theme(axis.text.x = element_text(angle = 90, hjust = 1))
+  plotlist[[k]]=p
+  k=k+1
+}
+
+pdf("RawTPMPlotsSortedBase_50.pdf")
+for (i in 1:length(plotlist)){
+  write(i, stderr())
+  print(plotlist[[i]])
+}
+dev.off()
+
+###separate tpms of Ens genes and others
+
+enstNames<-df_TPM$Name[(grepl("ENST",df_TPM$Name))]
+
+df_TPM_enst<-df_TPM[df_TPM$Name %in% enstNames,]
+
+df_TPM_nonenst<-df_TPM[!(df_TPM$Name %in% enstNames),]
+#sort by bases
+df_TPM_enst<-df_TPM_enst[,c('Name',bases$srr)]
+df_TPM_nonenst<-df_TPM_nonenst[,c('Name',bases$srr)]
+
+plotlist = list()
+k=1
+for(i in seq(2, dim(df_TPM_enst)[2], by = 200)){
+  df_s<-df_TPM_enst[,c(i:min(i+200,dim(df_TPM_enst)[2]))]+1
+  p<-ggplot(data=stack(df_s), aes(x=ind, y=(values))) + geom_crossbar(stat="summary", fun.y=data_summary, fun.ymax=getlogmax, fun.ymin=getlogmin) +theme(axis.text.x = element_text(angle = 90, hjust = 1))
+  plotlist[[k]]=p
+  k=k+1
+}
+pdf("RawTPMPlots_Enst.pdf")
+for (i in 1:length(plotlist)){
+  write(i, stderr())
+  print(plotlist[[i]])
+}
+dev.off()
+
+plotlist = list()
+k=1
+for(i in seq(2, dim(df_TPM_enst)[2], by = 200)){
+  df_s<-df_TPM_enst[,c(i:min(i+200,dim(df_TPM_enst)[2]))]+1
+  p<-ggplot(data=stack(df_s), aes(x=ind, y=(values))) + geom_crossbar(stat="summary", fun.y=data_summary, fun.ymax=getlogmax, fun.ymin=getlogmin) +theme(axis.text.x = element_text(angle = 90, hjust = 1))
+  plotlist[[k]]=p
+  k=k+1
+}
+pdf("RawTPMPlots_Enst.pdf")
+for (i in 1:length(plotlist)){
+  write(i, stderr())
+  print(plotlist[[i]])
+}
+dev.off()
+
+
+
+############normalize###################
 gene <- read.csv("transcript_names.txt")
 #Define the gene name if your transcript name is different from gene name
 tx2gene <- data.frame(GENEID=gene,TXNAME=gene)
