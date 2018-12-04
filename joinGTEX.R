011e06966a046f5911e89a10911545d15896ff39
Urminder Singh <mrbains21@gmail.com>
2018-12-04 17:57:52 -0600

    joinGtex tables

---
+++ joinGTEX.R
@@ -0,0 +1,29 @@
+library(readr)
+library(dplyr)
+
+getSubjID<-function(sampIds){
+  res<-c()
+  for(s in sampIds){
+    #print(s)
+    res<-c(res,paste(unlist(strsplit(s,"-"))[1],unlist(strsplit(s,"-"))[2],sep="-"))
+  }
+  return(res)
+}
+
+#join GTEX tables
+
+GTEXSA <- read_delim("D:/MOGdata/mog_testdata/cancer/raw/combined/tcgaMetadataALL/gtex/GTEx_v7_Annotations_SampleAttributesDS.txt", 
+                                                     "\t", escape_double = FALSE, trim_ws = TRUE)
+GTEXSP <-read_delim("D:/MOGdata/mog_testdata/cancer/raw/combined/tcgaMetadataALL/gtex/GTEx_v7_Annotations_SubjectPhenotypesDS.txt", 
+           "\t", escape_double = FALSE, trim_ws = TRUE)
+
+#mutate GTEXSA to add SUBJID
+x<-sapply(GTEXSA[,1],FUN = getSubjID)
+colnames(x)<-"SUBJID"
+GTEXSA<-cbind(GTEXSA[,],x)
+t<-GTEXSA[,c("SUBJID","SAMPID")]
+
+#join
+joined<-left_join(GTEXSP,GTEXSA)
+
+write_tsv(joined,"GTEX_merged.txt")
