00345498f6c28adf075cd2afb67ebc99fb49b219
Urminder Singh <mrbains21@gmail.com>
2018-12-24 19:53:39 -0600

    Add files via upload

---
+++ combineBrainAtlasMA.R
@@ -0,0 +1,23 @@
+library(dplyr)
+library(readr)
+
+
+
+
+
+MicroarrayExpression <- read_csv("~/Downloads/humanBrainAtlas/microarray/normalized_microarray_donor9861/MicroarrayExpression.csv", col_names = FALSE)
+
+Probes <- read_csv("~/Downloads/humanBrainAtlas/microarray/normalized_microarray_donor9861/Probes.csv")
+
+SampleAnnot <- read_csv("~/Downloads/humanBrainAtlas/microarray/normalized_microarray_donor9861/SampleAnnot.csv")
+#well id is uniq
+colnames(MicroarrayExpression)[1]<-"probe_id"
+colnames(MicroarrayExpression)[2:ncol(MicroarrayExpression)]<-SampleAnnot$well_id
+
+MicroarrayExpression_joined<-inner_join(Probes,MicroarrayExpression)
+
+sum(duplicated(colnames(MicroarrayExpression_joined)))
+
+write_tsv(MicroarrayExpression_joined,"JoinedData.tsv",col_names = F)
+
+
